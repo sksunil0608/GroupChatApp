0d26e6bb222f6592d5a7413f119b8934ea32bee1
sksunil0608 <sksunil0608@gmail.com>
2023-11-07 00:31:03 +0530

    Update codition, no chat Message

--- routes/messages.js
+++ routes/messages.js
@@ -5,22 +5,25 @@ const router = express.Router();
 router.post("/", (req, res, next) => {
     
     const message = req.body;
-    fs.appendFileSync("message.txt",`${message.username}:${message.message}`);
-    res.redirect('/');
+    // fs.appendFileSync("message.txt",`${message.username}:${message.message}`);
+    fs.writeFile("message.txt", `${message.username}:${message.message}`,{flag:'a'},(err)=>{
+        return err?console.log(err):res.redirect("/")
+    });
 });
 
 router.get("/", (req,res,next)=>{
     fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
       if (err) {
         console.log(err);
+        data = " Chat Not initiated"
       }
-    res.send(`<html>
+        res.send(`<html>
             <head>
                 <title>Group Chat</title>       
             </head>
             <body>
                 <h1> Messages </h1>
-                <p>${data}</p>
+                <p>${data?data:"No Messages"}</p>
                 <form action='/' onsubmit ="document.querySelector('#user').value =localStorage.getItem('user')" method ="POST">
                     <input type ="text" name="message">
                     <input id="user" type="hidden" name="username">
